ded0d010e81053ed6fbc924ae078d4bf51d5c97d
Work <Work@MSI>
2023-10-24 19:24:50 -0500

    Finished stacks in middle of class on Oct 24

--- Topic3-2436-H01/Source.cpp
+++ Topic3-2436-H01/Source.cpp
@@ -5,43 +5,102 @@
 using namespace std;
 
 
-void doSomethingElse()
+//void doSomethingElse()
+//{
+//    cout << "Doing something else ..." << endl; 
+//}
+//
+//void doSomething()
+//{
+//    doSomethingElse(); 
+//}
+
+bool isMatch(char openSymbol, char closeSymbol)
 {
-    cout << "Doing something else ..." << endl; 
+    if (openSymbol == '(' && closeSymbol == ')')
+    {
+        return true;
+    }
+    else if (openSymbol == '{' && closeSymbol == '}')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-void doSomething()
+
+bool isBalanced(string inputString)
 {
-    doSomethingElse(); 
-}
+    std::stack<char> openSymbolStack;
+    for (auto& currentChar : inputString) {
+        //check if current char is opening symbol
+        if (currentChar == '(' || currentChar == '{') {
+            openSymbolStack.push(currentChar);
+        }
+
+        //check for closing symbol:
+        else if (currentChar == ')' || currentChar == '}') {
+            if (openSymbolStack.empty())
+            {
+                return false; //close symbol MUST be preceded by an open symbol
+            }
+            if (isMatch(openSymbolStack.top(), currentChar))
+            {
+                openSymbolStack.pop();
+            }
+            else { //isMatch returned false, so string is unbalanced
+                return false;
+            }
+        }// add else (neither an open symbol nor a close symbol)
+    } //end for loop
+
+
+    if (!openSymbolStack.empty())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+} //end `isBalanced`
+
 
 int main()
 {
-    
-    //doSomething(); 
+ 
 
-    stack <string> namesStack; 
+    string UNbalancedExpression = "((){}";
 
-    namesStack.push("Alice");
-    namesStack.push("Bob");
-    namesStack.push("Carol");
+    cout << std::boolalpha << isBalanced(UNbalancedExpression) << endl; 
 
-    //
-    cout << namesStack.top() << endl; 
-    //The most important operations for stacks are top(), push(), and pop()
-    namesStack.pop(); 
-    cout << namesStack.size() << endl; 
-    namesStack.push("Darth");
+    ////doSomething(); 
 
-    cout << namesStack.top() << endl; 
+    //stack <string> namesStack; 
 
-    namesStack.pop();
-    namesStack.pop();
-    namesStack.pop();
-    if (!namesStack.empty())
-    {
-        namesStack.pop();
-    }
+    //namesStack.push("Alice");
+    //namesStack.push("Bob");
+    //namesStack.push("Carol");
+
+    ////
+    //cout << namesStack.top() << endl; 
+    ////The most important operations for stacks are top(), push(), and pop()
+    //namesStack.pop(); 
+    //cout << namesStack.size() << endl; 
+    //namesStack.push("Darth");
+
+    //cout << namesStack.top() << endl; 
+
+    //namesStack.pop();
+    //namesStack.pop();
+    //namesStack.pop();
+    //if (!namesStack.empty())
+    //{
+    //    namesStack.pop();
+    //}
 
 
     
